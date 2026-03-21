febed6c1d63019ae9c187e0b6c9385fa408f2861
Sam Deane <sam@elegantchaos.com>
2026-03-21 15:38:11 +0000

    Added workflow

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Runtime",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Runtime",
             targets: ["Runtime"]
         ),
     ],
+    dependencies: [
+      // Use `swift package --allow-writing-to-package-directory generate-workflow` to update the workflow for this package.
+      .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "2.1.3")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Runtime"
         ),
